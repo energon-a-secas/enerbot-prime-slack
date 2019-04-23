08809e3d3725695a8ede17267fa913a165f6edbf
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-23 15:43:39 -0400

    Rubocop

--- spec/unit/_weather_spec.rb
+++ spec/unit/_weather_spec.rb
@@ -2,7 +2,6 @@ require_relative '../spec_helper'
 require './senses/perception'
 
 describe '#Temperature' do
-
   context 'when thermal effect' do
     before(:each) do
       @dummy_class = DummyClass.new
@@ -12,6 +11,5 @@ describe '#Temperature' do
     it { expect(@dummy_class.thermal_delay('freeze')).to eq 2 }
 
     it { expect(@dummy_class.thermal_sensation_of(22)).to eq 'warm' }
-
   end
 end
