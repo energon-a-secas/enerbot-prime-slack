edc414f9c90fb7792b020c1c8cf0372df9b6ca19
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-23 10:50:35 -0400

    Add climatology sense

--- mind.rb
+++ mind.rb
@@ -1,3 +1,5 @@
+require './senses/perception'
+
 # I would like to use it as a "sense"
 module Conscious
   def configure_client(token = ENV['CL4P_API_TOKEN'])
@@ -8,16 +10,12 @@ module Conscious
 
     @client = Slack::RealTime::Client.new
   end
-
-  def space_perception # FIX THIS
-    25
-  end
 end
 
 # Base on the weather report, and should affect more actions than just the speed of response
 module Mood
-  extend Conscious
-  @weather = space_perception
+  extend Space_perception
+  @weather = weather_report
 
   def thermal_mood
     degrees = thermal_sensation_of(@weather)
@@ -25,25 +23,6 @@ module Mood
   end
 
   def event_mood; end
-
-  def thermal_sensation_of(degrees)
-    mood = { 0..10 => 'freeze',
-             11..19 => 'cold',
-             20..27 => 'warm',
-             28..33 => 'hot',
-             34..37 => 'burn' }
-    state = mood.select { |mood| mood === degrees }.values.first
-    thermal_delay(state)
-  end
-
-  def thermal_delay(state)
-    delay = { 'freeze' => 2,
-              'cold' => 0.8,
-              'warm' => 0.1,
-              'hot' => 0.5,
-              'burn' => 1 }
-    sleep(delay[state].to_i)
-  end
 end
 
 # Decisions
---
+++ senses/perception.rb
@@ -0,0 +1,25 @@
+# Cuz why not
+module Space_perception
+  def weather_report
+    25
+  end
+
+  def thermal_sensation_of(degrees)
+    mood = { 0..10 => 'freeze',
+             11..19 => 'cold',
+             20..27 => 'warm',
+             28..33 => 'hot',
+             34..37 => 'burn' }
+    state = mood.select { |mood| mood === degrees }.values.first
+    thermal_delay(state)
+  end
+
+  def thermal_delay(state)
+    delay = { 'freeze' => 2,
+              'cold' => 0.8,
+              'warm' => 0.1,
+              'hot' => 0.5,
+              'burn' => 1 }
+    sleep(delay[state].to_i)
+  end
+end
