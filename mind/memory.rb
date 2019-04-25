cb661bcae472dd10599b13cee910f3774c2220ca
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-25 16:00:15 -0400

    Update with method to destroy and erradicate collections

--- mind/memory.rb
+++ mind/memory.rb
@@ -2,19 +2,25 @@ require 'mongo'
 
 module Mongodb
 
-  def add(knowledge, registry = '', coll = 'ai', database = 'experience')
+  def add_document(knowledge, registry = '', coll = 'ai', database = 'experience')
     client = Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
-    collection = client["#{coll}"]
+    collection = client[coll]
     doc = { registry => knowledge }
     result = collection.insert_one(doc)
     result.n
   end
 
-  def remove(knowledge, registry, coll = 'rspec_coll', database = 'rspec_db')
+  def delete_document(knowledge, registry, coll = 'rspec_coll', database = 'rspec_db')
     client = Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
-    collection = client["#{coll}"]
+    collection = client[coll]
     result = collection.delete_one( { registry => knowledge} )
     result.deleted_count
+  end
+
+  def drop_collection(coll = 'rspec_coll', database = 'rspec_db')
+    client = Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
+    collection = client[coll]
+    collection.drop
   end
 
   def self.retrieve
--- spec/unit/_mongodb_spec.rb
+++ spec/unit/_mongodb_spec.rb
@@ -2,14 +2,16 @@ require_relative '../spec_helper'
 require './mind/memory'
 
 describe '#Mongodb' do
-  context 'when add entries' do
+  context 'when logged' do
     before(:each) do
       @dummy_class = DummyClass.new
       @dummy_class.extend(Mongodb)
     end
 
     # There's a need for a double check whenever exists or not a collection
-    it { expect(@dummy_class.add('Add one document', 'rspec_registry', 'rspec_coll', 'rspec_db')).to eq 1 }
-    it { expect(@dummy_class.remove('Add one document', 'rspec_registry', 'rspec_coll', 'rspec_db')).to eq 1 }
+    it { expect(@dummy_class.add_document('One document', 'rspec_registry', 'rspec_coll', 'rspec_db')).to eq 1 }
+    it { expect(@dummy_class.delete_document('One document', 'rspec_registry', 'rspec_coll', 'rspec_db')).to eq 1 }
+    # TODO Google how to evaluate this <Mongo::Operation::Result:
+    # it { expect(@dummy_class.drop_collection('rspec_coll', 'rspec_db')).to be_a_kind_of(String)}
   end
 end
