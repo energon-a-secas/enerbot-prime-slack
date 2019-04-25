ade13cd99a61a5c48e79554778984c9094669018
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-25 11:50:07 -0400

    Update with test for sight and fix for the twitter lookup

--- senses/sight.rb
+++ senses/sight.rb
@@ -1,18 +1,19 @@
 require 'twitter'
 
-module Eyes
-  config = {
-      consumer_key:        ENV['KEY'],
-      consumer_secret:     ENV['KEY_KEY'],
-      access_token:        ENV['TOKEN'],
-      access_token_secret: ENV['TOKEN_KEY']
-  }
+module Lookup
 
-  @client = Twitter::REST::Client.new(config)
+  def last_twitter(target)
+    config = {
+        consumer_key:        ENV['KEY'],
+        consumer_secret:     ENV['KEY_KEY'],
+        access_token:        ENV['TOKEN'],
+        access_token_secret: ENV['TOKEN_KEY']
+    }
 
-  def self.twitter_lookup(target)
+    client = Twitter::REST::Client.new(config)
+
     result = ''
-    @client.search("to:#{target}", result_type: 'recent').take(3).each do |tweet|
+    client.search("to:#{target}", result_type: 'recent').take(3).each do |tweet|
       result =+ "New tweet from #{tweet.user}: #{tweet.url}"
     end
     p result
