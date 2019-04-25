100cf304b04ebf69586447ebf2064b9467367dc9
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-25 16:30:46 -0400

    Update thanks to peyo

--- mind/memory.rb
+++ mind/memory.rb
@@ -2,30 +2,33 @@ require 'mongo'
 
 module Mongodb
 
-  def add_document(knowledge, registry = '', coll = 'ai', database = 'experience')
-    client = Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
+  def new_client(database)
+    Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
+  end
+
+  def add_document(knowledge, registry = '', coll = 'ai', db = 'experience')
+    client = new_client(db)
     collection = client[coll]
     doc = { registry => knowledge }
     result = collection.insert_one(doc)
     result.n
   end
 
-  def delete_document(knowledge, registry, coll = 'rspec_coll', database = 'rspec_db')
-    client = Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
+  def delete_document(knowledge, registry, coll = 'rspec_coll', db = 'rspec_db')
+    client = new_client(db)
     collection = client[coll]
     result = collection.delete_one( { registry => knowledge} )
     result.deleted_count
   end
 
-  def drop_collection(coll = 'rspec_coll', database = 'rspec_db')
-    client = Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
+  def drop_collection(coll = 'rspec_coll', db = 'rspec_db')
+    client = new_client(db)
     collection = client[coll]
     collection.drop
   end
 
-  def list_collections(database = 'rspec_db')
-    client = Mongo::Client.new(ENV['DATABASE_ADDRESS'], :database => database)
-    Mongo::Logger.logger.level = Logger::WARN
+  def list_collections(db = 'rspec_db')
+    client = new_client(db)
     database = client.database
     database.collection_names
   end
