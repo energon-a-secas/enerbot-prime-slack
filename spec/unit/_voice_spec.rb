dcdb9ac1128efee83fedd2833610e8a0d3e06d27
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-26 10:46:07 -0400

    Voice testing

---
+++ spec/unit/_voice_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../spec_helper'
+require './voice'
+
+describe '#Voice' do
+  context 'when post succeed' do
+    before(:each) do
+      @dummy_class = DummyClass.new
+      @dummy_class.extend(Voice)
+    end
+
+    it { expect(@dummy_class.normal_talk('Rspec message', '#bot_monitoring').to be_a_kind_of(Array)) }
+  end
+end
