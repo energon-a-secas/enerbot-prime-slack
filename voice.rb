f032cd29bf37144f7e6dbc23d408aa51962b1401
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-30 14:01:39 -0400

    Minor fix

--- voice.rb
+++ voice.rb
@@ -1,15 +1,13 @@
-require 'slack-ruby-client'
 require './mind/conscious'
 require './mind/judgment'
+
+USER_ICON = 'http://www.fritzhardy.com/games/borderlands_2_ps3/E19655605689AC6A24D6C76039CF748B4493BA1B.PNG'
+USER_NAME = 'CL4P-TP'
 
 # Message API
 module Voice
   include Conscious
   include Thought
-
-  USER_ICON = 'http://www.fritzhardy.com/games/borderlands_2_ps3/E19655605689AC6A24D6C76039CF748B4493BA1B.PNG'
-  USER_NAME = 'CL4P-TP'
-
 
   def normal_talk(text, data)
     client = configure_client
