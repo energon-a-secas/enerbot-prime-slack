3334f50e7c304c433b0524d1472565a3149cfb5a
Luciano Adonis <luciano.adonis@dxc.com>
2019-04-29 23:29:22 -0400

    I must sleep

--- voice.rb
+++ voice.rb
@@ -2,14 +2,14 @@ require 'slack-ruby-client'
 require './mind/conscious'
 require './mind/judgment'
 
-
-USER_NAME = 'http://www.fritzhardy.com/games/borderlands_2_ps3/E19655605689AC6A24D6C76039CF748B4493BA1B.PNG'
-USER_ICON = 'CL4P-TP'
-
 # Message API
 module Voice
   include Conscious
   include Thought
+
+  USER_ICON = 'http://www.fritzhardy.com/games/borderlands_2_ps3/E19655605689AC6A24D6C76039CF748B4493BA1B.PNG'
+  USER_NAME = 'CL4P-TP'
+
 
   def normal_talk(text, data)
     client = configure_client
