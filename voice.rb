48321275b99e1a82d58ca6b04e2583f5613f0cc8
Luciano Adonis <luciano.adonis@dxc.com>
2019-05-02 14:41:56 -0400

    Fix for single call from an independent module

--- voice.rb
+++ voice.rb
@@ -6,13 +6,16 @@ module Voice
   include Conscious
   include Thought
 
+  BOT_ICON = ENV['SLACK_BOT_ICON'] || 'https://i.imgur.com/eSCr8G3.png'
+  BOT_NAME = ENV['SLACK_BOT_NAME'] || 'Claptrap'
+
   def normal_talk(text, data)
     client = configure_client
     discern_end(data)
     client.web_client.chat_postMessage channel: @channel,
                                        text: text,
-                                       icon_url: ENV['SLACK_BOT_ICON'],
-                                       username: ENV['SLACK_BOT_NAME'],
+                                       icon_url: BOT_ICON,
+                                       username: BOT_NAME,
                                        thread_ts: @thread
   end
 
@@ -22,7 +25,7 @@ module Voice
     client.reactions_add channel: @channel,
                          name: text,
                          timestamp: @thread,
-                         icon_url: ENV['SLACK_BOT_ICON'],
-                         username: ENV['SLACK_BOT_NAME']
+                         icon_url: BOT_ICON,
+                         username: BOT_NAME
   end
 end
