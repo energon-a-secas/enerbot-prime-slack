eed1f136a1011a6abee2e77586e0d416f56ee32b
Luciano Adonis <luciano.adonis@dxc.com>
2019-05-06 12:23:52 -0400

    First step

--- directives.rb
+++ directives.rb
@@ -7,17 +7,30 @@ require './actions/retrieve'
 # Will does not refer to any particular desire, but rather to the mechanism for choosing from among one's directives.
 class Directive
 
-  def self.serve(data)
+  def self.listen(data)
+    text = data.text
+    case text
+    when /^\\/
+      Directive.serve(text, data)
+    when /aigis/
+      Directive.system(text, data)
+    end
+  end
+
+  def self.serve(text, data)
     func = { /eventos/ => System_status,
              /(bail[ea]|directive three)/ => Disco_dance,
              /canta/ => Sing_song,
              /recomienda algo/ => Recommend_song,
              /beneficio/ => Retrieve_benefit,
-             /fake status/ => Retrieve_status,
              /dame una excusa/ => Retrieve_excuse,
              /frase bronce/ => Retrieve_bronce,
              /dame un consejo/ => Retrieve_advice}
-    text = data.text
+    func.keys.any? { |key| func[key].exec(data) if key =~ text }
+  end
+
+  def self.system(text, data)
+    func = { /eventos/ => System_status }
     func.keys.any? { |key| func[key].exec(data) if key =~ text }
   end
 end
