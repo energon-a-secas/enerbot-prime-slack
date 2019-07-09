e3b7e4ae67919273d13b2d3cb55626a0fd9b1f86
Luciano Adonis <luciano.adonis@dxc.com>
2019-07-08 23:05:45 -0400

    Little fix

--- actions/enercoins.rb
+++ actions/enercoins.rb
@@ -28,7 +28,7 @@ module Enercoins
   end
 
   def self.update(user, type, data)
-    update_coins(user, type)
-    normal_talk("<@#{user}> has #{check_coins(user)} enercoins", data)
+    total_coins = update_coins(user, type)
+    normal_talk("<@#{user}> has #{total_coins} enercoins", data)
   end
 end
