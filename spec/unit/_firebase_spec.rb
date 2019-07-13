e32770d38cf98a83565bc74d768e709633478c29
Luciano Adonis <luciano.adonis@dxc.com>
2019-07-13 01:04:53 -0400

    Update unit testing with a few methods

--- spec/unit/_firebase_spec.rb
+++ spec/unit/_firebase_spec.rb
@@ -7,16 +7,13 @@ describe '#FireDB' do
   context 'when updates' do
     before(:each) do
       @dummy_class = DummyClass.new
-      @dummy_class.extend(FireOps)
+      @dummy_class.extend(FirebaseOps)
     end
 
-    it { expect(@dummy_class.check_coins('rspec_user')).to eq 1 }
-    it { expect(@dummy_class.update_coins('rspec_user', '++')).to eq 2 }
-    it { expect(@dummy_class.update_coins('rspec_user', '--')).to eq 1 }
-    it { expect(@dummy_class.update_coins('rspec_user', '++--')).to eq 1 }
-    it { expect(@dummy_class.update_coins('rspec_user', 'asdasd')).to eq 1 }
-    it { expect(@dummy_class.check_coins('rspec_user')).to eq 1 }
+    it { expect(@dummy_class.update_data('rspec_user', 2)).to eq true }
+    it { expect(@dummy_class.check_account('rspec_user')).to eq 2 }
+    it { expect(@dummy_class.update_data('rspec_user', -20)).to eq true }
+    it { expect(@dummy_class.check_account('rspec_user')).to eq -20 }
+    it { expect(@dummy_class.update_data('rspec_user', 2)).to eq true }
   end
 end
-
-
