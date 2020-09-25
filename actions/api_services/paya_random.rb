8fd79d07d3a2bd28ab8056ac2bb3d64c5b0a4987
Luciano Adonis <luciano.adonis@dxc.com>
2020-09-25 16:37:58 -0300

    Agrega paya

--- actions/api_services/paya_random.rb
+++ actions/api_services/paya_random.rb
@@ -6,7 +6,7 @@ module PayaRandom
 
   def self.exec(data)
     body = URI.open("https://payagenerator3000.herokuapp.com/").read
-    text = body.gsub('<h1> Paya Generator 3000 </h1>', "\n:flag-cl::flag-cl::flag-cl:\n>>>").gsub('<br>', "\n")
+    text = body.gsub('<h1> Paya Generator 3000 </h1>', "\n:flag-cl::flag-cl::flag-cl:\n").gsub('<br>', "\n")
     send_message(text, data)
   rescue OpenURI::HTTPError => e
     puts e.message
